56ca2f2b184a7dabd8c797760d79106420520494
Ajeeta Asthana <ajita.asthana@gmail.com>
2024-06-04 21:53:48 -0400

    Revert "todo app"
    This reverts commit 40c1a23eeaae6e1b7f935987e780e294e542624a.

--- todo-list/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ajeetaasthana/IdeaProjects/kernel/todo-list/build/_deps/googletest-src/googletest
-# Build directory: /Users/ajeetaasthana/IdeaProjects/kernel/todo-list/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- todo-list/build/test_TaskManager[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/Users/ajeetaasthana/IdeaProjects/kernel/todo-list/build/test_TaskManager[1]_tests.cmake")
-  include("/Users/ajeetaasthana/IdeaProjects/kernel/todo-list/build/test_TaskManager[1]_tests.cmake")
-else()
-  add_test(test_TaskManager_NOT_BUILT test_TaskManager_NOT_BUILT)
-endif()
