dcf57b3f945945dad6e474f0482f86f1e51bfe10
Ajeeta Asthana <ajita.asthana@gmail.com>
2025-03-16 13:07:25 -0400

    fork system call

--- algorithms/graph/fork.c
+++ algorithms/graph/fork.c
@@ -1,17 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<sys/wait.h>
 
 int main() {
-    pid_t child_pid = fork();
+    pid_t child_pid;
+    pid_t wait_result;
+    int stat_loc;
+
+    child_pid = fork();
 
     // The child process
     if(child_pid == 0) {
-        printf("### Child ### \n Current PID: %d and Child PID: %d\n", getpid(), child_pid);
+        printf("### Child ### \n Current PID: %d and Child PID: %d\n", 
+            getpid(), child_pid
+        );
+        sleep(1); // Sleep for one second
     } else {
-        printf("### Parent ### \n Current PID: %d and Child PID: %d \n", 
+        wait_result = waitpid(child_pid, &stat_loc, WUNTRACED);
+        printf("### Parent ###\n Current PID: %d and Child PID: %d\n",
             getpid(), child_pid
         );
     }
+
     return 0;
 }
